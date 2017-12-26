3eeb27d0d656bda75d0af329ea30848a93214e18
Niraj Ranjan <n.ranjan2808@gmail.com>
2017-12-27 00:10:35 +0530

    Database file in sql extension

---
+++ VOTING MANAGEMENT SYSTEM.sql
@@ -0,0 +1,50 @@
+﻿create table admin(
+
+name varchar(30),password varchar(30));
+
+insert into admin values ('niraj','ranjan');
+
+
+
+
+create table addvoter1 (
+voterid varchar(100) primary key ,
+password varchar(100),
+name varchar(100),
+fathername varchar(100),
+address varchar(200),
+sex varchar(100),
+age varchar(100)
+
+
+);
+
+select * from addvoter1
+
+delete result5;
+
+
+create table result5(
+id int,
+partyname varchar(100),
+count int 
+
+);
+
+insert into result5 values ('BJP',0,1)
+insert into result5 values ('INC',0,2)
+insert into result5 values ('CPI',0,3)
+insert into result5 values ('BSP',0,4)
+insert into result5 values ('NCP',0,5)
+
+
+
+
+create table partylist1(
+
+
+);
+
+
+
+select * from result5
